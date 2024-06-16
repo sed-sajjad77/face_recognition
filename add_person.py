e3bca090e2a410566fcd54d671a97be57e2fa5b1
saji <sedsajjad98@gmail.com>
2024-06-16 17:54:22 +0330

    Update add_person.py and README.md files

--- add_person.py
+++ add_person.py
@@ -5,7 +5,7 @@ import argparse
 
 
 class person:
-    def __init__(self ,dir_train ,dir_val, dir_test):
+    def __init__(self, dir_train, dir_val, dir_test):
         self.dir_train = dir_train
         self.dir_val = dir_val
         self.dir_test = dir_test
@@ -19,41 +19,43 @@ class person:
             ret, frame = cap.read()
 
             # Cut down frame to 250x250px
-            frame = frame[120:120+250,200:200+250, :]
-            
+            frame = frame[120:120+250, 200:200+250, :]
+
             if cv2.waitKey(1) & 0XFF == ord('a'):
 
                 if i < 40:
-                    # Create the unique file path 
-                    imgname = os.path.join(self.dir_train, '{}.jpg'.format(uuid.uuid1()))
+                    # Create the unique file path
+                    imgname = os.path.join(
+                        self.dir_train, '{}.jpg'.format(uuid.uuid1()))
                     # Write out train image
                     cv2.imwrite(imgname, frame)
 
                 elif i < 55:
-                    # Create the unique file path 
-                    imgname = os.path.join(self.dir_val, '{}.jpg'.format(uuid.uuid1()))
+                    # Create the unique file path
+                    imgname = os.path.join(
+                        self.dir_val, '{}.jpg'.format(uuid.uuid1()))
                     # Write out val images
                     cv2.imwrite(imgname, frame)
 
                 elif i < 65:
-                    # Create the unique file path 
-                    imgname = os.path.join(self.dir_test, '{}.jpg'.format(uuid.uuid1()))
+                    # Create the unique file path
+                    imgname = os.path.join(
+                        self.dir_test, '{}.jpg'.format(uuid.uuid1()))
                     # Write out test image
                     cv2.imwrite(imgname, frame)
 
                 else:
                     break
-                
-                i+=1
-            
-            
+
+                i += 1
+
             # Show image back to screen
             cv2.imshow('Image Collection', frame)
-            
+
             # Breaking gracefully
             if cv2.waitKey(25) & 0XFF == ord('q'):
                 break
-                
+
         # Release the webcam
         cap.release()
         # Close the image show frame
@@ -68,7 +70,7 @@ if __name__ == "__main__":
     parser.add_argument('--name', type=str, required=True)
     # Parse the argument
     args = parser.parse_args()
-    
+
     # create files
     if not os.path.exists('data'):
         os.mkdir('data')
@@ -90,7 +92,7 @@ if __name__ == "__main__":
         file.writelines(f'{args.name}\n')
         file.close()
 
-        #create dateset dir
+        # create dateset dir
         dir_path_train = os.path.join('data', 'train', args.name)
         dir_path_validation = os.path.join('data', 'validation', args.name)
         dir_path_test = os.path.join('data', 'test', args.name)
